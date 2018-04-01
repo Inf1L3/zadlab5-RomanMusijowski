d91c6ca69d449222a69629f9a6a944424063b2ca
Roman Local <rmusijowski@gmail.com>
2018-04-01 21:24:06 +0300

    Zadanie 3 i 5

---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Pracownik.java
@@ -0,0 +1,19 @@
+package pl.edu.ur.oopl5;
+
+public class Pracownik {
+
+
+    public String imie;
+    public String nazwisko;
+    public int wiek;
+    public String stanowisko;
+
+
+    public Pracownik(String imie, String nazwisko, int wiek, String stanowisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+        this.stanowisko = stanowisko;
+    }
+
+}
